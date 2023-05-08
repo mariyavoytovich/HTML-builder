7b6434b5fd4a9cb25d632234ae782ba5dd7fb217
mariyavoytovich <68275286+mariyavoytovich@users.noreply.github.com>
2023-05-08 22:31:19 +0300

    feat: task 06-build-page - copy assets implementation

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,11 +1,13 @@
 const path = require("path");
+const { constants } = require("fs");
 const { createWriteStream, createReadStream } = require("fs");
-const { mkdir } = require("fs/promises");
+const { mkdir, readdir, copyFile } = require("fs/promises");
 const { createInterface } = require("readline");
 
 //Folders
 const RESULT_FOLDER = "project-dist";
 const COMPONENTS_FOLDER = "components";
+const ASSETS_FOLDER = 'assets';
 
 const TEMPLATE_HTML = "template.html";
 const RESULT_HTML = "index.html";
@@ -18,6 +20,7 @@ createFolder(distFolderPath).then(() => {
   const templateHtmlPath = path.join(__dirname, TEMPLATE_HTML);
   const bundleHtmlPath = path.join(distFolderPath, RESULT_HTML);
   bundleHtml(templateHtmlPath, bundleHtmlPath);
+  copyAssets();
 });
 
 function createFolder(folderPath) {
@@ -54,3 +57,40 @@ function parseLine(line) {
   const isComponent = COMPONENT_PATTERN.test(line);
   return [isComponent, isComponent ? line.match(COMPONENT_PATTERN)[1] : line];
 }
+
+async function copyAssets(){
+  await copyFolder(ASSETS_FOLDER, __dirname, distFolderPath);
+}
+
+async function copyFolder(folderName, from, to){
+  const folderPath = path.join(from, folderName);
+  const copyFolderPath = path.join(to, folderName);
+  await createFolder(copyFolderPath);
+  await copyFolderContent(folderPath, copyFolderPath);
+}
+
+async function copyFolderContent(from, to){
+  const content = await readdir(from, {withFileTypes: true});
+  for(const component of content){
+    await copyFolderComponent(component, from, to);
+  }
+}
+
+function copyFolderComponent(component, from, to) {
+  const { name } = component;
+
+  if(component.isFile()){
+    return copyFolderFile(name, from, to);
+  }
+
+  if(component.isDirectory()){
+    return copyFolder(name, from, to);
+  }
+}
+
+function copyFolderFile(fileName, from, to){
+  const filePath = path.join(from, fileName);
+  const destinationPath = path.join(to, fileName);
+  return copyFile(filePath, destinationPath, constants.COPYFILE_FICLONE);
+}
+
